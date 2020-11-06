7a059e37a1a4de2e3c5ae354e59a306623a56acc
PRAAVUS <73282565+PRAAVUS@users.noreply.github.com>
2020-11-06 19:55:12 +0100

    Add project files.

---
+++ Challenge String and its methods 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Challenge_String_and_its_methods_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
